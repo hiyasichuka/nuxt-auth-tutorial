2a3a0e365c21284ef9466c4a558adf0dbd4f2eca
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-09-17 20:25:56 +0900

    setup github authenticate

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,13 @@
+// file: ~/server/api/auth/[...].ts
+import { NuxtAuthHandler } from '#auth'
+import GithubProvider from 'next-auth/providers/github'
+
+export default NuxtAuthHandler({
+  providers: [
+    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+    GithubProvider.default({
+      clientId: process.env.GH_CLIENT_ID,
+      clientSecret: process.env.GH_SECRET
+    })
+  ]
+})
