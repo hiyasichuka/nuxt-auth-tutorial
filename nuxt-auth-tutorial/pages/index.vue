dadc6e62914cdaee609743fd2a22f81a03af2398
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-09-26 13:04:59 +0900

    Add me get and example code

--- nuxt-auth-tutorial/pages/index.vue
+++ nuxt-auth-tutorial/pages/index.vue
@@ -2,10 +2,13 @@
   <div>
     <button v-if="loggedIn" @click="signOut()">SiginOut</button>
     <button v-else @click="signIn('github')">SiginIn</button>
+    <pre>{{ data.user }}</pre>
   </div>
 </template>
 <script setup lang="ts">
-const { status, data, signIn, signOut } = useAuth();
+const { status, signIn, signOut } = useAuth();
 
 const loggedIn = computed(() => status.value === "authenticated");
+
+const { data } = await useFetch("/api/me");
 </script>
