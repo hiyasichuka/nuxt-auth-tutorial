01e10679d6d7bf22d549bac4d7f23630fc5caa05
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-09-26 10:23:36 +0900

    Refactor authentication API to use AzureADProvider and implement token refreshing functionality

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,13 +1,60 @@
-// file: ~/server/api/auth/[...].ts
 import { NuxtAuthHandler } from '#auth'
-import GithubProvider from 'next-auth/providers/github'
-
-export default NuxtAuthHandler({
-  providers: [
-    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
-    GithubProvider.default({
-      clientId: process.env.GH_CLIENT_ID,
-      clientSecret: process.env.GH_SECRET
+import AzureADProvider from 'next-auth/providers/azure-ad'
+async function refreshAccessToken(accessToken) {
+  try {
+    const url = `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/oauth2/v2.0/token`
+    const req = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body:
+        `grant_type=refresh_token` +
+        `&client_secret=${process.env.AZURE_AD_CLIENT_SECRET}` +
+        `&refresh_token=${accessToken.refreshToken}` +
+        `&client_id=${process.env.AZURE_AD_CLIENT_ID}`
     })
-  ]
+    const res = await req.json()
+    return {
+      ...accessToken,
+      accessToken: res.access_token,
+      accessTokenExpires: Date.now() + res.expires_in * 1000,
+      refreshToken: res.refresh_token ?? accessToken.refreshToken // Fall back to old refresh token
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      ...accessToken,
+      error: 'RefreshAccessTokenError'
+    }
+  }
+}
+export default NuxtAuthHandler({
+  secret: process.env.NUXT_AUTH_SECRET,
+  providers: [
+    AzureADProvider.default({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+      authorization: {
+        params: {
+          scope: `offline_access openid profile email ${process.env.AZURE_AD_CLIENT_ID}/access_as_user`
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      // Persist the access_token in the encrypted JWT.
+      if (account && profile) {
+        token.accessToken = account.access_token
+        token.accessTokenExpires = account.expires_at * 1000
+        token.refreshToken = account.refresh_token
+      }
+      if (Date.now() < token.accessTokenExpires) {
+        return token
+      }
+      return refreshAccessToken(token)
+    }
+  }
 })
