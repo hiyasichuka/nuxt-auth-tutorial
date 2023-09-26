32a83f165c807979c9518f7a853874ae4251d006
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-09-26 11:09:11 +0900

    Refactor app.vue to include buttons for signing in and signing out, and handle the corresponding actions using the useAuth() function

--- nuxt-auth-tutorial/app.vue
+++ nuxt-auth-tutorial/app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <button v-if="loggedIn" @click="handleSignOut">SiginOut</button>
+    <button v-else @click="handleSignIn">SiginIn</button>
   </div>
 </template>
+<script setup lang="ts">
+const { status, data, signIn, signOut } = useAuth();
+
+const loggedIn = computed(() => status.value === "authenticated");
+
+async function handleSignIn() {
+  await signIn("github");
+}
+
+async function handleSignOut() {
+  await signOut();
+}
+</script>
