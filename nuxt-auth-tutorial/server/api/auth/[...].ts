92664033751615bc37233addcf48f2d7a4c57ff5
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-09-26 15:23:16 +0900

    Refactor code to use Prisma as the adapter for NextAuth, importing the necessary modules and initializing the PrismaClient

--- nuxt-auth-tutorial/server/api/auth/[...].ts
+++ nuxt-auth-tutorial/server/api/auth/[...].ts
@@ -1,7 +1,12 @@
 import { NuxtAuthHandler } from '#auth'
 import GithubProvider from 'next-auth/providers/github'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export default NuxtAuthHandler({
+  adapter: PrismaAdapter(prisma),
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
